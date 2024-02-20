ef19cdd5eafce82546eeca69bc3df0e51016efab
Nihal Kashinath <nihal.kashinath@gmail.com>
2024-02-20 23:04:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
+import streamlit as st
 from embedchain import App
+
+st.title("YouTube Podcast Bot!")
+st.subheader("Share a YouTube Podcast link and ask the bot questions about it")
 
 YTpodcast_bot = App()
 
 YTpodcast_bot.add("https://www.youtube.com/watch?v=yqfKsoDLff8")
-YTpodcast_bot.query("What is the summary of the video?")
-
+response = YTpodcast_bot.query("What is the summary of the video?")
+st.write(response)
